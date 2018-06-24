27dc442a204c8683173e7c5188bdd389d85bcadd
Yaroslav <justyarik@gmail.com>
2018-06-24 17:37:53 +0300

    sdafsdsss

--- index15.js
+++ index15.js
@@ -1,9 +1,7 @@
 console.log('sldkfjf');
 console.log('sldkfjf');
-console.log('sldkfjf');
-console.log('sldkfjf');
-console.log('sldkfjf');
-console.log('sldkfjf');
-console.log('sldkfjf');
-console.log('sldkfjf');
-console.log('sldkfjf');
+
+// slkdfj
+// askdjfa
+// asdkfja
+// asdkfja
