3168ab73a48ebb90b3d594a430c923ee7473d8f9
Podoba.i <justyarik@gmail.com>
2018-07-06 19:11:04 +0300

    fuel Login working

--- DBfunctions.js
+++ DBfunctions.js
@@ -16,11 +16,11 @@ con.connect((err) => {
     console.log('MySQL Connected...');
 });
 
-exports.getUser = function (callback, suserLogin, userPassword) {
+exports.getSysUserLogin = function (callback, suserLogin, userPassword) {
     // console.log('getData function');
 
-    let sqlreq = "SELECT * FROM sysUsers WHERE upper(sUserLogin) = upper('"+suserLogin+"')" +
-        " AND sUserPasswordHash ='"+userPassword+"'";
+    let sqlreq = "SELECT * FROM sysUsers WHERE upper(sUserLogin) = upper('" + suserLogin + "')" +
+        " AND sUserPasswordHash ='" + userPassword + "'";
     con.query(sqlreq, (err, result) => {
         // console.log('request string');
         // console.log(sqlreq);
@@ -38,7 +38,48 @@ function returnData(data){
 }
 
 
-exports.newone = function () { console.log('new one'); };
+exports.getClientLogin = function (callback, clientLogin, clientPassword) {
+    // console.log('getData function');
+
+    let sqlreq = "SELECT * FROM clients WHERE upper(clientLogin) = upper('" + clientLogin + "')" +
+        " AND clientPassword ='" + clientPassword + "'";
+    con.query(sqlreq, (err, result) => {
+        // console.log('request string');
+        // console.log(sqlreq);
+        if (err) throw err;
+        // console.log('result form DM');
+        // console.log(result);
+        callback(returnData(result));
+    });
+};
+exports.makeNerOrder = function (callback, clientEmail ,orderQuantity, orderFuelType, orderPStype) {
+    // console.log('getData function');
+
+    let sqlreq = " INSERT INTO orders (orderClientID, orderName, orderDate, orderQuontity, orderFuelType, orderPatrolStationType ) \n"+
+    " SELECT                                                        \n"+
+    " (                                                             \n"+
+    "     SELECT clientID FROM clients                              \n"+
+    "     WHERE upper(clientLogin) = upper('" + clientEmail + "')   \n"+
+    "     LIMIT 1                                                   \n"+
+    " )                                                             \n"+
+    "    , 'newOrder'                                               \n"+
+    "    , now()                                                    \n"+
+    "    , " + orderQuantity.toString() + "                         \n"+
+    "    , " + orderFuelType.toString() + "                         \n"+
+    "    , " + orderPStype.toString() + "                           \n"+
+    ";";
+    console.log(sqlreq);
+    
+    con.query(sqlreq, (err, result) => {
+        // console.log('request string');
+        // console.log(sqlreq);
+        if (err) throw err;
+        // console.log('result form DM');
+        // console.log(result);
+        // callback(returnData(result));
+    });
+};
+// exports.newone = function () { console.log('new one'); };
 
 // getData(function (res) {
 //     console.log();// EXPORT
--- fuelMain.js
+++ fuelMain.js
@@ -17,27 +17,75 @@ app.get('/', function (req, res) {
 
 app.post('/login', urlencoderParser, function (req, result) {
     if (!req.body) return result.sendStatus(400);
-    // console.log(req.body); // { email: 'JustYarik@gmail.com', pass: '1' }
+    console.log(req.body); // { email: 'JustYarik@gmail.com', pass: '1' }
 
-    if (req.body.email)  {
-            dbFunctions.getUser(function (res) {
+    if (req.body.email && req.body.sysUserCheck)  {
+            dbFunctions.getSysUserLogin(function (res) {
             // console.log(res);        // do not remove
             if (res.length !== 0) {
-                console.log('for user '+ req.body.email+ ' access ALLOWED');
-                result.render('about-success' , {data: req.body});
+                console.log('for system user '+ req.body.email+ ' access ALLOWED');
+                result.render('sysUserCabinet' , {data: req.body});
             }
             else {
-                result.send('Login or password are wrong')
-                console.log('for user '+ req.body.email+ ' access DENIED');
+                result.send('Login or password are wrong');
+                console.log('for system user '+ req.body.email+ ' access DENIED');
             }
         }
         , req.body.email
         , req.body.pass );
     }
+
+    if (req.body.email && !req.body.sysUserCheck)  {
+        dbFunctions.getClientLogin(function (res) {
+                // console.log(res);        // do not remove
+                if (res.length !== 0) {
+                    console.log('for user '+ req.body.email+ ' access ALLOWED');
+                    result.render('clientCabinet' , {data: req.body});
+                }
+                else {
+                    result.send('Login or password are wrong');
+                    console.log('for user '+ req.body.email+ ' access DENIED');
+                }
+            }
+            , req.body.email
+            , req.body.pass );
+    }
 });
 
+app.post('/newOrderFromClient', urlencoderParser, function (req, result) {
+    if (!req.body) return result.sendStatus(400);
+    let ft ='';
+    if (req.body.f95){ft = '95'};
+
+    let psType ='';
+    if (req.body.psWOG) {psType = 1} else { psType = 2}
 
 
+    console.log('new order');
+    console.log(req.body);
+    // result.send('order accepted')
+    if (req.body.orderQuantity && (req.body.psWOG || req.body.psWOG ) ) {
+        dbFunctions.makeNerOrder(function (res) {
+                console.log(res);
+            }
+            , req.body.email
+            , req.body.orderQuantity
+            , ft
+            , psType
+        );
+        result.render('clientCabinet', {data: req.body});
+    }
+
+});
+
+// function fuelTypeSelector(ft){
+//    let fft = '';
+//    switch (ft) {
+//        case 'f95': fft = 95; break;
+//        case 'f92': fft = 92; break;
+//     }
+//     return fft;
+// }
 
 // app.post('/about', urlencoderParser, function (req, res) {
 //     if (!req.body) return res.sendStatus(400);
